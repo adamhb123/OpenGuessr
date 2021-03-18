109570deb783058dada07669280d3f804216f23f
Adam Brewer <adamhb321@gmail.com>
2021-03-18 12:28:47 -0400

    Init

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CSHGuessr' });
+});
+
+module.exports = router;
