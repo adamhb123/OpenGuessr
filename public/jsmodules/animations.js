77a446cd3a06c19b4a1363cd59e45313ad19289f
Adam Brewer <adamhb321@gmail.com>
2021-04-21 21:24:43 -0400

    Animations work now

--- public/jsmodules/animations.js
+++ public/jsmodules/animations.js
@@ -1,24 +1,8 @@
 //  Can automate frame retrieval by searching anim folder
-function preload(frame){
-  let im = new Image();
-  im.src = frame;
-  return im;
-}
-
 const EFFECTS = {
-  "sparkle":[
-    preload("/images/animations/sparkle/frame_00.png"),
-    preload("/images/animations/sparkle/frame_01.png"),
-    preload("/images/animations/sparkle/frame_02.png"),
-    preload("/images/animations/sparkle/frame_03.png"),
-    preload("/images/animations/sparkle/frame_04.png"),
-    preload("/images/animations/sparkle/frame_05.png"),
-    preload("/images/animations/sparkle/frame_06.png"),
-    preload("/images/animations/sparkle/frame_07.png"),
-    preload("/images/animations/sparkle/frame_08.png"),
-    preload("/images/animations/sparkle/frame_09.png")
-  ]
+  "sparkle": "/images/animations/sparkle.png"
 };
+
 var animationCanvas = document.createElement("canvas");
 animationCanvas.className = "animation-canvas";
 animationCanvas.width = document.body.clientWidth;
@@ -26,19 +10,43 @@ animationCanvas.height = document.body.clientHeight;
 document.body.append(animationCanvas);
 var animationCtx = animationCanvas.getContext("2d");
 
-function animate(effect, x, y){
-  console.log("Drawing animation");
-  let frames = EFFECTS[effect];
+function Animatable(spritesheet, x, y, width, height, animationSpeed){
+  this.spritesheet = new Image();
+  this.spritesheet.src = spritesheet;
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.scaleWidth = width;
+  this.scaleHeight = height;
+  this.animationSpeed = animationSpeed;
+  this.setScale = (_width,_height) => {
+    this.scaleWidth = _width;
+    this.scaleHeight = _height;
+  }
+}
+
+function animate(animatable){
   let i = 0;
+  let start = Date.now();
   let intid = setInterval(()=>{
-    if(i == frames.length) clearInterval(intid);
-    animationCtx.clearRect(0, 0, animationCtx.canvas.width, animationCtx.canvas.height)
-    animationCtx.drawImage(frames[i],x,y); // Or at whatever offset you like
-    i++;
-  },100);
+    let timeElapsed = Date.now() - start;
+    if(i == 10) clearInterval(intid);
+    animationCtx.clearRect(0, 0, animationCtx.canvas.width, animationCtx.canvas.height);
+    // img, sx, sy, swidth, sheight, x, y, width, height
+    animationCtx.drawImage(animatable.spritesheet, animatable.width*i,0,animatable.width,animatable.height,
+      animatable.x - (animatable.scaleWidth / 2), animatable.y - (animatable.scaleWidth / 2), animatable.scaleWidth, animatable.scaleWidth);
+    if(timeElapsed >= animatable.animationSpeed){
+      i++;
+      start = Date.now();
+    }
+  }, Math.floor(animatable.animationSpeed/2));
 };
+
 
 export function sparkle(x, y){
   //console.log(`Sparkle anim @ (${x},${y}) on ctx ${animationCtx
-  window.addEventListener("load" , animate.bind(null,"sparkle",x,y) , false);
+  let animatable = new Animatable(EFFECTS.sparkle, x, y, 32, 32, 32);
+  //animatable.setScale(16,16);
+  animatable.spritesheet.addEventListener("load" , animate.bind(null, animatable) , false);
 }
